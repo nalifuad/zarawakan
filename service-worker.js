7db02f80304122b9005642420e4f3f0a6eddcff3
nalifuad <kuzunokun@gmail.com>
2025-02-07 18:47:27 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,22 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')  // Points to the root location
-    .then(function(registration) {
-      console.log('Service Worker registered with scope: ', registration.scope);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/styles.css',  // Add this only if you have a styles.css file
+        '/icon.png',
+        '/icon-512.png'
+      ]);
     })
-    .catch(function(error) {
-      console.log('Service Worker registration failed: ', error);
-    });
-}
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
